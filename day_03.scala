60f351c842c28301f54f759ea010cc5da125037e
Ben Kirzhner <ben.kirzhner@gmail.com>
2022-12-02 22:33:51 -0800

    Day 3 Part 2

--- day_03.scala
+++ day_03.scala
@@ -4,11 +4,14 @@ object Day03 {
   @main def main() = {
     val lines = Source.fromFile("day_03.input").getLines().toList
     println(
-      f"Part 1: Total priorities of duplicated items: ${lines.map(priorityOfDuplicatedItem).sum}"
+      f"Part 1: Total priorities of duplicated items: ${lines.map(duplicatedItemInRucksack).map(itemPriority).sum}"
+    )
+    println(
+      f"Part 2: Total priorities of common items across: ${lines.grouped(3).map(duplicatedItemAcrossRucksacks).map(itemPriority).sum}"
     )
   }
 
-  def priorityOfDuplicatedItem(line: String): Int = {
+  def duplicatedItemInRucksack(line: String): Char = {
     val (firstPart, secondPart) = line.splitAt(line.length / 2)
     val firstCompartmentItems: Set[Char] = firstPart.toSet
     val secondCompartmentItems: Set[Char] = secondPart.toSet
@@ -16,7 +19,7 @@ object Day03 {
     val commonItems: Set[Char] =
       firstCompartmentItems.intersect(secondCompartmentItems)
     commonItems.toList match {
-      case List(item: Char) => charPriority(item)
+      case List(item: Char) => item
       case _ =>
         throw new IllegalArgumentException(
           f"Unexpectedly found multiple duplicated items in rucksack: ${line}, dupes: ${commonItems}"
@@ -24,13 +27,24 @@ object Day03 {
     }
   }
 
-  def charPriority(char: Char): Int = {
-    if ('a' <= char && char <= 'z') {
-      char.toInt - 'a' + 1
-    } else if ('A' <= char && char <= 'Z') {
-      char.toInt - 'A' + 27
+  def duplicatedItemAcrossRucksacks(lines: List[String]): Char = {
+    val commonItems: Set[Char] = lines.map(_.toSet).reduce(_.intersect(_))
+    commonItems.toList match {
+      case List(item: Char) => item
+      case _ =>
+        throw new IllegalArgumentException(
+          f"Unexpectedly found multiple duplicated items in rucksack set: ${lines}, dupes: ${commonItems}"
+        )
+    }
+  }
+
+  def itemPriority(item: Char): Int = {
+    if ('a' <= item && item <= 'z') {
+      item.toInt - 'a' + 1
+    } else if ('A' <= item && item <= 'Z') {
+      item.toInt - 'A' + 27
     } else {
-      throw new IllegalArgumentException(f"Unexpected character: ${char}")
+      throw new IllegalArgumentException(f"Unexpected character: ${item}")
     }
   }
 }
