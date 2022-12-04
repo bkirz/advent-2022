4481f9721fb18008e60f605af03480ee2146964c
Ben Kirzhner <ben.kirzhner@gmail.com>
2022-12-04 06:44:56 -0800

    Day 3 cleanup

--- day_03.scala
+++ day_03.scala
@@ -3,39 +3,36 @@ import scala.io.Source
 object Day03 {
   @main def main() = {
     val lines = Source.fromFile("day_03.input").getLines().toList
-    println(
-      f"Part 1: Total priorities of duplicated items: ${lines.map(duplicatedItemInRucksack).map(itemPriority).sum}"
-    )
-    println(
-      f"Part 2: Total priorities of common items across: ${lines.grouped(3).map(duplicatedItemAcrossRucksacks).map(itemPriority).sum}"
-    )
+
+    val dupedItemsPerRucksackPriority =
+      lines
+        .map(duplicatedItemInRucksack)
+        .map(itemPriority)
+        .sum
+
+    val dupedItemsAcrossTeamsPriority =
+      lines
+        .grouped(3)
+        .map(duplicatedItemAcrossRucksacks)
+        .map(itemPriority)
+        .sum
+
+    println(f"Part 1: ${dupedItemsPerRucksackPriority}")
+    println(f"Part 2: ${dupedItemsAcrossTeamsPriority}")
   }
 
   def duplicatedItemInRucksack(line: String): Char = {
-    val (firstPart, secondPart) = line.splitAt(line.length / 2)
-    val firstCompartmentItems: Set[Char] = firstPart.toSet
-    val secondCompartmentItems: Set[Char] = secondPart.toSet
-
-    val commonItems: Set[Char] =
-      firstCompartmentItems.intersect(secondCompartmentItems)
-    commonItems.toList match {
-      case List(item: Char) => item
-      case _ =>
-        throw new IllegalArgumentException(
-          f"Unexpectedly found multiple duplicated items in rucksack: ${line}, dupes: ${commonItems}"
-        )
-    }
+    val (firstCompartment: Set[Char], secondCompartment: Set[Char]) =
+      line.splitAt(line.length / 2).map(_.toSet)
+    val commonItems: Set[Char] = firstCompartment.intersect(secondCompartment)
+    val List(item: Char) = commonItems.toList
+    item
   }
 
   def duplicatedItemAcrossRucksacks(lines: List[String]): Char = {
     val commonItems: Set[Char] = lines.map(_.toSet).reduce(_.intersect(_))
-    commonItems.toList match {
-      case List(item: Char) => item
-      case _ =>
-        throw new IllegalArgumentException(
-          f"Unexpectedly found multiple duplicated items in rucksack set: ${lines}, dupes: ${commonItems}"
-        )
-    }
+    val List(item: Char) = commonItems.toList
+    item
   }
 
   def itemPriority(item: Char): Int = {
